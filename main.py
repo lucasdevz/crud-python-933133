86838fe3fc4f1037b2de075535e140ca737eb19d
lucas <85170796+lucasdevz@users.noreply.github.com>
2024-10-17 17:38:54 +0000

    salvar

--- main.py
+++ main.py
@@ -1,77 +1,94 @@
-# .txt
-# banco de dados
-#SQLite
-
-#SQL = Linguagem de consulta estruturada
-#ORM (para SQL) CREATE DATABASE meu banco
-# i/o # i = input(entrada) #o = output(saída)
-
-# Select * from clientes
-#nome, sobrenome, idade
-
-#ORM = biblioteca p esconder comandos como o select, na sua linguagem
-# pip install sqlalchemy
-
 import os
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Criando banco de dados
+# Criando banco de dados.
 MEU_BANCO = create_engine("sqlite:///meubanco.db")
 
-# Meio dos dois, um cria e o outro comunica
+# Criando conexão com banco de dados.
+Session = sessionmaker(bind=MEU_BANCO)
+session = Session()
 
-#criando conexão com banco de dados
-session = sessionmaker(bind=MEU_BANCO)
-session = session()
 
 # Criando tabela.
 
-base = declarative_base()
+Base = declarative_base()
 
-class Usuario(base):
+class Usuario(Base):
     __tablename__ = "usuarios"
 
-
-    # Definindo campos da tabela
+    # Definindo campos da tabela.
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     nome = Column("nome", String)
     email = Column("email", String)
     senha = Column("senha", String)
 
-    # Definindo atributos da classe
-    def _init_(self, nome: str, email: str, senha: str):
-        self.name = nome
+    # Definindo atributos da classe.
+    def __init__(self, nome: str, email: str, senha: str):
+        self.nome = nome
         self.email = email
         self.senha = senha
 
-# Criando tabela no banco de dados
-base.metadata.create_all(bind=MEU_BANCO)
+# Criando tabela no banco de dados.
+Base.metadata.create_all(bind=MEU_BANCO)
 
-# Salvar no banco de dados
+# Salvar no banco de dados.
 os.system("cls || clear")
 
 # Create
 print("Solicitando dados para o usuário")
-inserir_nome=input("Digite seu nome")
-inserir_email=input("Digite seu  email")
-inserir_senha=input("Digite sua senha")
+inserir_nome = input("Digite seu nome: ")
+inserir_email = input("Digite seu e-mail: ")
+inserir_senha = input("Digite seu senha ")
 
-usuario = Usuario (nome=inserir_nome, email=inserir_email, senha=inserir_senha)
+usuario = Usuario(nome=inserir_nome, email=inserir_email, senha=inserir_senha)
 session.add(usuario)
 session.commit()
 
-usuario = Usuario(nome="Maria", email="maria@gmail.com", senha="456")
-session.add(usuario)
-session.commit()
-
-# Listando todos os usuários do banco de dados
-print("\nExibindo todos os usuários do banco de dados.")
+# Listando todos os usuários do banco de dados.
+# Read
+print("\nExibindo todos os usuários do bando de dados.")
 lista_usuarios = session.query(Usuario).all()
 
-#Read
 for usuario in lista_usuarios:
-    print(f"{usuario.id} - {usuario.nome} - {usuario.senha}")
+    print(f"{usuario.id} - {usuario.nome} - {usuario.email} - {usuario.senha}")
 
-#Fechando conexão.
+# Delete
+print("\nExcluindo um usuário.")
+email_usuario = input("Informe o email do usuário para ser excluido: ")
+
+usuario = session.query(Usuario).filter_by(email = email_usuario).first()
+session.delete(usuario)
+session.commit()
+print(f"{usuario.nome} excluido com sucesso.")
+
+# Listando todos os usuários do banco de dados.
+# Read
+print("\nExibindo todos os usuários do bando de dados.")
+lista_usuarios = session.query(Usuario).all()
+
+for usuario in lista_usuarios:
+    print(f"{usuario.id} - {usuario.nome} - {usuario.email} - {usuario.senha}")
+
+# Update
+print("\nAtualizando dados do usuário.")
+email_usuario = input("Informe o email do usuário que será atualizado: ")
+
+usuario = session.query(Usuario).filter_by(email = email_usuario).first()
+
+usuario.nome = input("Digite seu nome: ")
+usuario.email = input("Digite seu e-mail: ")
+usuario.senha = input("Digite seu senha: ")
+
+session.commit()
+
+# Listando todos os usuários do banco de dados.
+# Read
+print("\nExibindo todos os usuários do bando de dados.")
+lista_usuarios = session.query(Usuario).all()
+
+for usuario in lista_usuarios:
+    print(f"{usuario.id} - {usuario.nome} - {usuario.email} - {usuario.senha}")
+
+# Fechando conexão.
 session.close()
